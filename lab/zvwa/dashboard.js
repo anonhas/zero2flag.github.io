332ecda13ee2bfc2c9dfdc74d72313dbca041a99
Ana Elizaur <37194208+anonhas@users.noreply.github.com>
2025-05-23 10:31:53 -0300

    Update dashboard.js

--- lab/zvwa/dashboard.js
+++ lab/zvwa/dashboard.js
@@ -1,29 +1,24 @@
-const user = JSON.parse(localStorage.getItem("user"));
 
+const user = JSON.parse(localStorage.getItem("user"));
 if (!user) {
   alert("Acesso negado. Faça login primeiro.");
   window.location.href = "login.html";
 }
-
 const role = user.role;
-
 if (role === "admin") {
   document.getElementById("painel-admin").style.display = "block";
   document.getElementById("botao-secreto").style.display = "block";
-
+  document.getElementById("painel-master").style.display = "block";
   fetch("https://zvwa-api.onrender.com/api/dashboard/data", {
     headers: { "x-role": "admin" }
-  })
-    .then(res => res.json())
+  }).then(res => res.json())
     .then(data => {
       document.getElementById("dados-admin").textContent = JSON.stringify(data, null, 2);
     });
-
 } else {
   document.getElementById("painel-user").style.display = "block";
   document.getElementById("botao-secreto").style.display = "block";
 }
-
 function acessarSegredo() {
   fetch("https://zvwa-api.onrender.com/api/dashboard/secret-data", {
     headers: { "x-role": role }
@@ -31,12 +26,10 @@ function acessarSegredo() {
     .then(async res => {
       const data = await res.json();
       const destino = document.getElementById("segredo");
-
       if (!res.ok) {
-        destino.textContent = `Erro: ${data.error}`;
+        destino.textContent = "Erro: " + data.error;
         return;
       }
-
       destino.textContent = JSON.stringify(data, null, 2);
     });
 }
